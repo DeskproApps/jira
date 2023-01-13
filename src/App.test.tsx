f50894bfda13be0a81fb2958d23f2d1e8711d9b0
Tweeno <itweeno@gmail.com>
2023-01-13 13:39:10 +0000

    Changed to PNPM and React 18

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,39 @@
 import { render, waitFor } from "@testing-library/react";
 import App from "./App";
+import * as React from "react";
+import fetch from "node-fetch";
+
+jest.mock("@deskpro/app-sdk", () => ({
+  ...jest.requireActual("@deskpro/app-sdk"),
+  useDeskproAppEvents: (hooks: any, deps: [] = []) => {
+    const deskproAppEventsObj = {
+      data: {
+        ticket: {
+          id: 1,
+          subject: "Test Ticket",
+        },
+      },
+    };
+    React.useEffect(() => {
+      hooks.onChange && hooks.onChange(deskproAppEventsObj);
+      hooks.onShow && hooks.onShow(deskproAppEventsObj);
+      hooks.onReady && hooks.onReady(deskproAppEventsObj);
+    }, deps);
+  },
+  useInitialisedDeskproAppClient: (callback: any) => {
+    callback({
+      registerElement: () => {},
+      deregisterElement: () => {},
+    });
+  },
+  proxyFetch: async () => fetch,
+}));
 
 test("renders App component", async () => {
-    const { getByText } = render(<App />);
+  const { getByText } = render(<App />);
 
-    await waitFor(() => {
-        const headingElement = getByText(/Ticket Data/i);
-        expect(headingElement).toBeInTheDocument();
-    });
+  await waitFor(() => {
+    const headingElement = getByText(/Ticket Data/i);
+    expect(headingElement).toBeInTheDocument();
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import { DeskproAppProvider } from "@deskpro/app-sdk";
+import * as React from "react";
 import { Main } from "./pages/Main";
-import "./App.css";
-
-import "flatpickr/dist/themes/light.css";
-import "tippy.js/dist/tippy.css";
-import "simplebar/dist/simplebar.min.css";
-
-import "@deskpro/deskpro-ui/dist/deskpro-ui.css";
-import "@deskpro/deskpro-ui/dist/deskpro-custom-icons.css";
 
 function App() {
   return (
-      <DeskproAppProvider>
-        <Main />
-      </DeskproAppProvider>
+    <DeskproAppProvider>
+      <Main></Main>
+    </DeskproAppProvider>
   );
 }
 
