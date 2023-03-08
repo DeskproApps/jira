2b61e90d13a311816e29872c25f4abe20fc1e632
Jake Biddell <jake.biddell@deskpro.com>
2023-03-08 15:06:16 +0000

    Add deskpro-ui styles imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import "@deskpro/deskpro-ui/dist/deskpro-ui.css";
+import "@deskpro/deskpro-ui/dist/deskpro-custom-icons.css";
 import { DeskproAppProvider } from "@deskpro/app-sdk";
 import * as React from "react";
 import { Main } from "./pages/Main";
