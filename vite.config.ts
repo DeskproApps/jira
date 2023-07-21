fe4f0f28dd8edb0e9753707f89a529144b07a92e
ITweeno <itweeno@gmail.com>
2023-07-21 14:44:37 +0100

    silenced module level directories warning.

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,12 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
+          return;
+        }
+        warn(warning);
+      },
       plugins: [
         copy({
           hook: "writeBundle",
@@ -20,6 +26,6 @@ export default defineConfig({
           ],
         }),
       ],
-    }
+    },
   },
 });
