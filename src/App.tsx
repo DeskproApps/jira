5a85ab8eb57faf25fbe1220e1d50c0faf7659b6d
ilia.makarov <ilia.makarov@me.com>
2024-04-24 16:51:45 +0300

    Rollback iframe-resizer

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,21 @@ import {
   QueryErrorResetBoundary,
 } from "@tanstack/react-query";
 import { ErrorBoundary } from "react-error-boundary";
-
 import { StoreProvider } from "./context/StoreProvider/StoreProvider";
 import { Main } from "./pages/Main";
-import "./App.css";
-
-import "flatpickr/dist/themes/light.css";
-import "tippy.js/dist/tippy.css";
-import "simplebar/dist/simplebar.min.css";
-
-import "@deskpro/deskpro-ui/dist/deskpro-ui.css";
-import "@deskpro/deskpro-ui/dist/deskpro-custom-icons.css";
-
 import TimeAgo from "javascript-time-ago";
 import en from "javascript-time-ago/locale/en.json";
 import { AdminSettings } from "./pages/AdminPage";
 import { ErrorFallback } from "./components/ErrorFallback/ErrorFallback";
+
+import "iframe-resizer/js/iframeResizer.contentWindow.js";
+import "./App.css";
+import "flatpickr/dist/themes/light.css";
+import "tippy.js/dist/tippy.css";
+import "simplebar/dist/simplebar.min.css";
+import "@deskpro/deskpro-ui/dist/deskpro-ui.css";
+import "@deskpro/deskpro-ui/dist/deskpro-custom-icons.css";
+
 
 TimeAgo.addDefaultLocale(en);
 
