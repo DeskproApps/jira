71aa72990cf1f2873138804ca8188871867a1b5d
Paul Happy Hutchinson <paul.hutchinson@deskpro.com>
2024-08-21 13:09:23 +0100

    [Feature] SC-124017 Refactor apps to remove iframe-resizer dependency and make iframe content sizing more robust

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,6 @@ import "./App.css";
 import "flatpickr/dist/themes/light.css";
 import "simplebar/dist/simplebar.min.css";
 import "tippy.js/dist/tippy.css";
-import "iframe-resizer/js/iframeResizer.contentWindow.js";
 
 import {
   LoadingSpinner,
