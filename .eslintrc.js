8c6df3564d5905dbd4dcafb12fb19ad0b5c3bca6
tidbit1 <194808961+tidbit1@users.noreply.github.com>
2025-01-23 17:01:47 +0000

    use correct plugin for TypeScript ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
   plugins: ["@typescript-eslint"],
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended-type-checked",
+    'plugin:@typescript-eslint/recommended',
     "plugin:react-hooks/recommended",
     "prettier",
   ],
