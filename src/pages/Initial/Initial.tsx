6d6e467cf898da0870856faa6a9549ad1d33f84b
tidbit1 <194808961+tidbit1@users.noreply.github.com>
2025-03-14 09:21:44 +0000

    ensure loaded context for OAuth2 check, update manifest

--- src/pages/Initial/Initial.tsx
+++ src/pages/Initial/Initial.tsx
@@ -1,16 +1,29 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { LoadingSpinner, useDeskproLatestAppContext, useInitialisedDeskproAppClient } from '@deskpro/app-sdk';
+import { checkIsAuth } from '../../api/checkIsAuth';
 import { CLOUD_ID_PATH, IS_USING_OAUTH2 } from '../../constants';
 import { Settings } from '../../types';
-import { checkIsAuth } from '../../api/checkIsAuth';
 
 export function Initial() {
     const { context } = useDeskproLatestAppContext<unknown, Settings>();
+    const [hasContextLoaded, setHasContextLoaded] = useState(false);
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const navigate = useNavigate();
 
+    useEffect(() => {
+        if (!context) {
+            return;
+        };
+
+        setHasContextLoaded(true);
+    }, [context]);
+
     useInitialisedDeskproAppClient(async client => {
+        if (!hasContextLoaded) {
+            return;
+        };
+
         const isUsingOAuth2 = context?.settings.use_api_key !== true;
         
         await client.setUserState(IS_USING_OAUTH2, isUsingOAuth2);
@@ -32,7 +45,7 @@ export function Initial() {
                     navigate('/log_in');
                 });
         };
-    }, [context]);
+    }, [context, hasContextLoaded]);
 
     useEffect(() => {
         if (isLoggedIn) {
